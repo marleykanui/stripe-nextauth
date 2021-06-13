730c84da43be880186dc16f7105e908b1d48d313
Kanui <marley.kanui@gmail.com>
2021-06-13 01:55:15 -0500

    stripe nextauth - global commit 15 - finally passed a successful checkout session

--- use-shopping-cart-stripe/pages/api/checkout_sessions/cart.ts
+++ use-shopping-cart-stripe/pages/api/checkout_sessions/cart.ts
@@ -1,8 +1,6 @@
 // Stripe
 import Stripe from 'stripe';
 
-// // Use-Shopping-Cart
-// import { validateCartItems } from 'use-shopping-cart/src/serverUtil';
 // Custom
 import { validateCartItems } from '@/utils/1-stripe/validateCartItems';
 
@@ -19,35 +17,22 @@ export default async function handler(
 ) {
   if (req.method === 'POST') {
     try {
-      // Validate the cart details that were sent from the client.
       const cartItems = req.body;
       const productInventory = await stripe.prices.list({
         active: true,
         limit: 10,
         expand: ['data.product'],
       });
+
       const line_items = validateCartItems(
         productInventory.data as any,
         cartItems
       );
 
-      // console.log(JSON.stringify(productInventory.data, null, 2));
-      // console.log('##########################################');
-      // console.log(JSON.stringify(cartItems, null, 2));
-
-      console.log(cartItems);
-      // console.log(values(cartItems[0]));
-
       const params: Stripe.Checkout.SessionCreateParams = {
         mode: 'payment',
         payment_method_types: ['card'],
-        line_items: [{}],
-
-        // [
-        //   { price: Object.keys(cartItems)[0], quantity: 2 },
-        //   { price: Object.keys(cartItems)[1], quantity: 2 },
-        //   { price: Object.keys(cartItems)[2], quantity: 2 },
-        // ],
+        line_items,
         success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${req.headers.origin}/shoppingcart`,
       };
--- use-shopping-cart-stripe/utils/1-stripe/validateCartItems.ts
+++ use-shopping-cart-stripe/utils/1-stripe/validateCartItems.ts
@@ -1,9 +1,75 @@
 // Types
-interface InventoryItemProps {}
+interface ProductProps {
+  id: string;
+  object: string;
+  active: boolean;
+  attributes: [];
+  created: number;
+  description: string;
+  images: string[];
+  livemode: boolean;
+  metadata: {};
+  name: string;
+  package_dimensions?: {};
+  shippable?: boolean;
+  statement_descriptor?: string;
+  type: string;
+  unit_label?: string;
+  updated: number;
+  url?: string;
+}
 
-interface CartItemProps {}
+interface InventoryItemProps {
+  id: string;
+  object: string;
+  active: boolean;
+  billing_scheme: string;
+  created: number;
+  currency: string;
+  livemode: boolean;
+  lookup_key?: string;
+  metadata: {};
+  nickname: string;
+  product: ProductProps;
+  recurring?: {};
+  tiers_mode?: string;
+  transform_quantity?: {};
+  type: string;
+  unit_amount: number;
+  unit_amount_decimal: string;
+}
+
+interface CartItemProps {
+  name: string;
+  description: string;
+  id: string;
+  currency: string;
+  price: number;
+  image: string;
+  quantity: number;
+  value: number;
+  formattedValue: string;
+}
 
 export const validateCartItems = (
   inventory: InventoryItemProps[],
   cart: CartItemProps[]
-) => {};
+) => {
+  const validatedCartItems = [];
+
+  for (const itemId in cart) {
+    const product = cart[itemId];
+    const inventoryItem = inventory.find(
+      (currentProduct) => currentProduct.id === itemId
+    );
+
+    if (!inventoryItem) throw new Error(`Product ${itemId} not found!`);
+
+    const item = {
+      price: inventoryItem.id,
+      quantity: product.quantity,
+    };
+    validatedCartItems.push(item);
+  }
+  return validatedCartItems;
+};
