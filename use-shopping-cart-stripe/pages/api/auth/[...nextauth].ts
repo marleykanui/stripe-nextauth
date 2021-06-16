dbcc12823a58d5d8fee218745444b51c1fca526b
Kanui <marley.kanui@gmail.com>
2021-06-16 00:33:10 -0500

    stripe nextauth - global commit 21 - listing providers for next-auth

--- use-shopping-cart-stripe/pages/api/auth/[...nextauth].ts
+++ use-shopping-cart-stripe/pages/api/auth/[...nextauth].ts
@@ -3,5 +3,28 @@ import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
 export default NextAuth({
-  providers: [],
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    Providers.Facebook({
+      clientId: process.env.FACEBOOK_CLIENT,
+      clientSecret: process.env.FACEBOOK_SECRET,
+    }),
+    Providers.Twitter({
+      clientId: process.env.TWITTER_CLIENT,
+      clientSecret: process.env.TWITTER_SECRET,
+    }),
+    Providers.Email({
+      server: {
+        host: '',
+        port: 0,
+        auth: {
+          user: '',
+          pass: '',
+        },
+      },
+    }),
+  ],
 });
