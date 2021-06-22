3010ee3bea02bb72af859d265dcbed8d6964eb7b
KANUI <marley.kanui@gmail.com>
2021-06-22 13:51:31 -0500

    stripe nextauth - global commit 25 - succesfully added needed data to mongodb

--- use-shopping-cart-stripe/pages/api/checkout_sessions/[id].ts
+++ use-shopping-cart-stripe/pages/api/checkout_sessions/[id].ts
@@ -1,13 +1,13 @@
 // Stripe
-import Stripe from 'stripe';
+import Stripe from "stripe";
 
 // Stripe Instance
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2020-08-27',
+  apiVersion: "2020-08-27",
 });
 
 // Next Types
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
@@ -15,13 +15,15 @@ export default async function handler(
 ) {
   const id: string = req.query.id as string;
   try {
-    if (!id.startsWith('cs_')) {
-      throw Error('Incorrect CheckoutSession ID.');
+    if (!id.startsWith("cs_")) {
+      throw Error("Incorrect CheckoutSession ID.");
     }
-    const checkout_session: Stripe.Checkout.Session =
-      await stripe.checkout.sessions.retrieve(id, {
-        expand: ['payment_intent'],
-      });
+    const checkout_session: Stripe.Checkout.Session = await stripe.checkout.sessions.retrieve(
+      id,
+      {
+        expand: ["payment_intent", "line_items"],
+      }
+    );
 
     res.status(200).json(checkout_session);
   } catch (err) {
--- use-shopping-cart-stripe/pages/sessionconfirm.tsx
+++ use-shopping-cart-stripe/pages/sessionconfirm.tsx
@@ -1,28 +1,29 @@
 // React
-import { useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 // Next
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
 // Axios
-import axios from 'axios';
+import axios from "axios";
 
 // SWR
-import useSWR from 'swr';
+import useSWR from "swr";
 
 // Use-Shopping-Cart
-import { useShoppingCart } from 'use-shopping-cart';
+import { useShoppingCart } from "use-shopping-cart";
 
 // Components
-import Layout from '@/components/layout/3-components/Layout';
-import Cart from '@/components/cart/3-components/Cart';
-import PrintObject from '@/components/cart/3-components/PrintObject';
-import ClearCart from '@/components/cart/3-components/ClearCart';
+import Layout from "@/components/layout/3-components/Layout";
+import Cart from "@/components/cart/3-components/Cart";
+import PrintObject from "@/components/cart/3-components/PrintObject";
+import ClearCart from "@/components/cart/3-components/ClearCart";
 
 // Next Types
-import { NextPage } from 'next';
+import { NextPage } from "next";
 
 const ResultPage: NextPage = () => {
+  const [DBUpdated, setDBUpdated] = useState(false);
   const router = useRouter();
 
   const getSessionData = async (sessionUrl: string) => {
@@ -34,58 +35,58 @@ const ResultPage: NextPage = () => {
     }
   };
 
-  const dbUpdateWithNewSession =
-    // const { data, error } = useSWR(
-    //   router.query.session_id
-    //     ? `/api/checkout_sessions/${router.query.session_id}`
-    //     : null,
-    //   getSessionData
-    // );
+  const { data, error } = useSWR(
+    router.query.session_id
+      ? `/api/checkout_sessions/${router.query.session_id}`
+      : null,
+    getSessionData
+  );
 
-    // const setCustomerId = async () => {
-    //   getSessionData(
-    //     router.query.session_id
-    //       ? `/api/checkout_sessions/${router.query.session_id}`
-    //       : null
-    //   );
-    //   // try {
-    //   await axios.post('/api/db/createCustomer', {
-    //     customer_id: sessionData.customer,
-    // customer_email: data.customer_email,
-    // customer_name: data.charges.data[0].billing_details.name,
-    // customer_transactions: [
-    //   {
-    //     transaction_id: data.charges.data[0].id,
-    //     transaction_date: Date.now(),
-    //     transaction_receipt_url: data.charges.data[0].receipt_url,
-    //     payment_method_type:
-    //       data.charges.data[0].payment_method_details.type,
-    //     currency: data.charges.data[0].currency,
-    //     transaction_amount: data.charges.data[0].amount_captured,
-    //   },
-    // ],
-    // });
-    // } catch (error) {
-    //   console.error(error);
-    // }
-    // };
+  const dbUpdateWithNewSession = async (sessionData: any) => {
+    const {
+      data: { customer, customer_email, payment_intent, line_items },
+    } = sessionData;
+    try {
+      await axios.post("/api/db/createCustomer", {
+        customer_id: customer,
+        customer_email: customer_email,
+        customer_name: payment_intent.charges.data[0].billing_details.name,
+        customer_transactions: [
+          {
+            transaction_id: payment_intent.charges.data[0].id,
+            transaction_date: Date.now(),
+            transaction_receipt_url: payment_intent.charges.data[0].receipt_url,
+            payment_method_type:
+              payment_intent.charges.data[0].payment_method_details.type,
+            currency: payment_intent.charges.data[0].currency,
+            purchasedProductData: line_items.data,
+          },
+        ],
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    useEffect(() => {
-      setCustomerId();
-    }, []);
+  useEffect(() => {
+    if (data && !DBUpdated) {
+      dbUpdateWithNewSession(data);
+      setDBUpdated(true);
+    }
+  }, [data]);
 
   if (error) return <div>failed to load</div>;
 
   return (
     <Layout title="Checkout Payment Result | Next.js + TypeScript Example">
       <div className="page-container">
-        <h1>Checkout Payment Result</h1>
-        <h2>Status: {data?.data.payment_intent?.status ?? 'loading...'}</h2>
+        {/* <h1>Checkout Payment Result</h1>
+        <h2>Status: {data?.data.payment_intent?.status ?? "loading..."}</h2>
         <h3>CheckoutSession response:</h3>
-        <PrintObject content={data?.data ?? 'loading...'} />
-        <Cart>
+        <PrintObject content={data?.data ?? "loading..."} /> */}
+        {/* <Cart>
           <ClearCart />
-        </Cart>
+        </Cart> */}
       </div>
     </Layout>
   );
