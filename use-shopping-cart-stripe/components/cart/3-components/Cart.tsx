1f5b15b366b91131254f101dc758406b6fb718d4
KANUI <marley.kanui@gmail.com>
2021-07-27 08:14:43 -0500

    stripe nextauth - global commit 27 - working solution - pre update

--- use-shopping-cart-stripe/components/cart/3-components/Cart.tsx
+++ use-shopping-cart-stripe/components/cart/3-components/Cart.tsx
@@ -1,26 +1,28 @@
 // Use-Shopping-Cart
-import { CartProvider } from 'use-shopping-cart';
+import { CartProvider } from "use-shopping-cart";
 
 // Stripe Instance
-import getStripe from '@/utils/3-stripe/getStripe';
+import getStripe from "@/utils/3-stripe/getStripe";
 
 // Cart Config Params
-import * as config from '@/config/index';
+import * as config from "@/config/index";
 
 // React Types
-import { FC } from 'react';
+import { FC } from "react";
 
 // Component Level Types
-import { CartProps } from '../0-types/CartProps';
+import { CartProps } from "../0-types/CartProps";
 
-const Cart: FC<CartProps> = ({ children }) => (
-  <CartProvider
-    mode="checkout-session"
-    stripe={getStripe()}
-    currency={config.CURRENCY}
-  >
-    <>{children}</>
-  </CartProvider>
-);
+const Cart: FC<CartProps> = ({ children }) => {
+  return (
+    <CartProvider
+      mode="checkout-session"
+      stripe={getStripe()}
+      currency={config.CURRENCY}
+    >
+      <>{children}</>
+    </CartProvider>
+  );
+};
 
 export default Cart;
